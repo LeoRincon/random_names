7abb22e90fecd8bb940222ace83dde8bccf950e5
Leonardo Rincon <jleobotache@gmail.com>
2021-02-03 11:58:36 -0500

    inicio del proyecto con npm

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const names = [
+  "Luis Marino",
+  "Carlos Leiva",
+  "Mstha Lex",
+  "Mark Luxer",
+  "Anais Luci",
+  "Camila New",
+  "Lucas Mario",
+  "Leonor Castro",
+  "Maria Letter"
+]
+
+//Crear función para enviar aleatoriamente  los nombres del arreglo
+const nameRandom = () => {
+  const name = names[Math.floor(Math.random() * names.length)]
+  console.log(name)
+};
+
+// Exportar como un módulo
+
+module.exports = { nameRandom };
